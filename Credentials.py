d26f19eee804eda887c0d5fa9c0d2ebf77fe97d5
klara.saary <klara.saary@stud.tu-darmstadt.de>
2025-07-01 19:33:43 +0200

    Introduce Credential.py for placing session api and X-XSRF-TOKEN

---
+++ Credentials.py
@@ -0,0 +1,8 @@
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+    "X-XSRF-TOKEN": "[YOUR_XSRF_TOKEN]",  # <-- Hier dein XSRF-Token eintragen nach dem Login
+}
+COOKIES = {
+    "SESSION": "[YOUR_SESSION_KEY]", # <-- Hier dein Session-Cookie eintragen nach dem Login
+    "XSRF-TOKEN": "[YOUR_XSRF_TOKEN]" # <-- Hier dein XSRF-Cookie eintragen nach dem Login
+}
